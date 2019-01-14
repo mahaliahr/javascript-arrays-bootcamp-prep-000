0b758412306e90781972627cf111adde7da74564
Mahalia Henry-Richards <mahalia_hr@outlook.com>
2019-01-14 18:42:34 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,20 +1,23 @@
 var chocolateBars = ["snickers", "hundred grand", "kitkat", "skittles"]
 
 function addElementToBeginningOfArray(array, element) {
-
+array = [element, ...array]
+return array
 }
 
 function destructivelyAddElementToBeginningOfArray(array, element) {
-var array = new Array
-array.push(element)
+array.unshift(element)
+return array
 }
 
 function addElementToEndOfArray(array, element) {
-
+  array = [array, ...element]
+  return array
 }
 
 function destructivelyAddElementToEndOfArray(array, element) {
-
+array.push(element)
+return array
 }
 
 function accessElementInArray(array, element) {
